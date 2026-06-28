e2a8f9e51a4a485d15189f1fc0bad116743ebed5
Nate Beck <>
2026-06-28 13:38:35 -0500

    feat(providers): add file provider abstraction

--- src/frameflow/providers/__init__.py
+++ src/frameflow/providers/__init__.py
@@ -1,1 +1,9 @@
-"""Package marker. SPDX-License-Identifier: Apache-2.0"""
+"""Provider abstractions."""
+
+from .models import FileCandidate
+from .protocols import FileProvider
+
+__all__ = [
+    "FileCandidate",
+    "FileProvider",
+]
